8beba2e06902b4850d664311e168e4affaf720e9
Balazs Balatincz <baszik.99@gmail.com>
2021-06-17 15:53:45 +0200

    :*

--- add.js
+++ add.js
@@ -3,11 +3,15 @@ function handleSubmit(event) {
   event.preventDefault();
   
   
-  
+  //adatok felbontása
   existing = existing ? JSON.parse(existing) : {};
+  //Formból származó adatok kinyerése
+  //topikok külön tömbbe, mintsem külön mentve
   const data = new FormData(event.target);
   const value = Object.fromEntries(data.entries());
   value.topics = data.getAll("topics");
+  //Az létező adatok hosza
+  //ezt növelve nem felülír, hanem hozzátesz
   var cr = Object.keys(existing).length;
   
   cr++;
@@ -17,7 +21,7 @@ function handleSubmit(event) {
 
 }
 
-//localStorage.clear(); 
+//localStorage.clear(); //ezcsaktesztelésmiattvanitt 
   const form = document.querySelector('form');
   form.addEventListener('submit', handleSubmit);  
   
